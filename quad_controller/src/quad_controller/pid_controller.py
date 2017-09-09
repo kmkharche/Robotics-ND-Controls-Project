959ffb0c25f95da28b3e03f01d7b99790e22edf8
Ketan Kharche <ketan3393@gmail.com>
2017-09-09 14:23:50 -0700

    fix:Edit reset function of PID controller

--- quad_controller/src/quad_controller/pid_controller.py
+++ quad_controller/src/quad_controller/pid_controller.py
@@ -31,7 +31,10 @@ class PIDController:
         self.kd_ = 0.0
         self.ki_ = 0.0
         self.set_point_ = 0.0
-        self.max_windup_ = 0.0
+        #self.max_windup_ = 0.0
+        self.last_timestamp_ = 0.0
+        self.error_sum_ = 0.0
+        self.last_error_ = 0.0
 
     def setTarget(self, target):
         #TODO
@@ -65,9 +68,9 @@ class PIDController:
         p = self.kp_*error
         
         self.error_sum_ += error*delta_time
-        if sel.error_sum_>self.max_windup_:
+        if self.error_sum_>self.max_windup_:
         	self.error_sum_ = self.max_windup_
-        i=self.ki_*self.error_sum_
+        i = self.ki_*self.error_sum_
         
 
         error_diff = (error - self.last_error_)/delta_time
@@ -76,5 +79,11 @@ class PIDController:
         u = p + i + d
 
         self.last_timestamp_ = timestamp
+        self.last_error_ = error
+        
+        self.u_p.append(p)
+        self.u_i.append(i)
+        self.u_d.append(d)
 
+        return u
 
